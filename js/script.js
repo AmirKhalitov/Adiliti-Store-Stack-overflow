654ca3316d889d37d6d451ad65534cac1cc8e506
Amir Khalitov <64722976+AmirKhalitov@users.noreply.github.com>
2020-09-28 10:29:05 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.banner__main').slick({
+        slidesToShow: 1,
+        infinite: true,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: false,
+        autoplaySpeed: 800,
+        speed: 5000,
+        draggable: false,
+        dots: true,
+        variableWidth: true,
+    });
+});
